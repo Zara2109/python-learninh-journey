0b4f6f1904ab8a344f1b480d9cef6642624deca0
Zahrah <zahrahmukarram@gmail.com>
2026-07-21 22:06:32 +0530

    variables notes

---
+++ variables/variables.py
@@ -0,0 +1,16 @@
+# String
+name = "Zahrah"
+
+# Integer
+age = 26
+
+# Float
+weight = 96.0
+
+# Boolean
+is_learning_python = True
+
+print("Name:", name)
+print("Age:", age)
+print("Weight:", weight)
+print("Learning Python:", is_learning_python)
